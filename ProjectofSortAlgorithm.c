7f9f9cd840c65e7c7d88272a2da5677fb014bb76
Showkatulislam <64518592+Showkatulislam@users.noreply.github.com>
2021-01-26 23:56:31 +0600

    Add files via upload

--- ProjectofSortAlgorithm.c
+++ ProjectofSortAlgorithm.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+//This is swep function.
 void swep(int *a,int *b)
 {
     int temp;
@@ -6,107 +7,7 @@ void swep(int *a,int *b)
     *a=*b;
     *b=temp;
 }
-void Bubblesort(int a[],int n)
-{
-    int i,j;
-    for(i=0;i<n-1;i++)
-    {
-        for(j=0;j<n-1-i;j++)
-        {
-            if(a[j]>a[j+1])
-                swep(&a[j],&a[j+1]);
-        }
-    }
-}
-void Selectionsort(int a[],int n)
-{
-    int i,j,min;
-    for(i=0;i<n-1;i++)
-    {
-        min=i;
-        for(j=i;j<n;j++)
-        {
-            if(a[j]<a[min])
-                min=j;
-        }
-        swep(&a[i],&a[min]);
-    }
-}
-void Insertionsort(int a[],int n)
-{
-    int i,j,x;
-    for(i=1;i<n;i++)
-    {
-        x=a[i];
-        j=i-1;
-        while(j>-1 && a[j]>x)
-        {
-           a[j+1]=a[j];
-           j--;
-        }
-        a[j+1]=x;
-    }
-}
-
-int partition(int a[],int l,int h)
-{
-    int pivot,i,j;
-    pivot=a[l];
-    i=l,j=h;
-    while(i<j)
-    {
-        do{
-            i++;
-        }while(a[i]<=pivot);
-        do{
-            j--;
-        }while(a[j]>pivot);
-        if(i<j)
-        swep(&a[i],&a[j]);
-    }
-    swep(&a[l],&a[j]);
-    return j;
-}
-void Quicksort(int a[],int l,int h)
-{
-    int j;
-    if(l<h)
-    {
-        j=partition(a,l,h);
-        Quicksort(a,l,j);
-        Quicksort(a,j+1,h);
-    }
-}
-void Marge(int a[],int l,int mid,int h)
-{
-    int i=l,j=mid+1,k=l;
-    int b[100];
-    while(i<=mid && j<=h)
-    {
-        if(a[i]<a[j])
-            b[k++]=a[i++];
-        else
-            b[k++]=a[j++];
-
-    }
-    for(;i<=mid;i++)
-        b[k++]=a[i];
-    for(;j<=h;j++)
-        b[k++]=a[j];
-    for(i=l;i<=h;i++)
-        a[i]=b[i];
-}
-void Margesort(int a[],int l,int h)
-{
-    int mid;
-    if(l<h)
-    {
-        mid=(l+h)/2;
-        Margesort(a,l,mid);
-        Margesort(a,mid+1,h);
-        Marge(a,l,mid,h);
-    }
-}
+//This Max function
 int Max(int a[],int n)
 {
     int i,max;
@@ -118,40 +19,7 @@ int Max(int a[],int n)
     }
     return max;
 }
-void Countsort(int a[],int n)
-{
-    int i,j,max;
-    max=Max(a,n)+1;
-    int c[max];
-    for(i=0;i<max;i++)
-        c[i]=0;
-    for(i=0;i<n;i++)
-        c[a[i]]++;
-    i=0,j=0;
-    while(i<max)
-    {
-        if(c[i]>0)
-        {
-            a[j++]=i;
-            c[i]--;
-        }
-       else
-            i++;
-    }
-}
-void print()
-{
-    printf("If want to exit enter 0 time....\n");
-    printf("Enter for 1 Bubble sort.O(n^2)..\n");
-    printf("Enter for 2 Selection sort O(n^2).\n");
-    printf("Enter for 3 Insertion sort.O(n^2).\n");
-    printf("Enter for 4 Quick sort.nlogn.\n");
-    printf("Enter for 5 Marge sort.nlogn;\n");
-    printf("Enter for 6 Count sort..O(n).\n");
-    printf("Enter for 7 Enter new array.\n");
-
-
-}
+//This is display function of array.
 void display(int a[],int n)
 {
     int i;
@@ -159,6 +27,7 @@ void display(int a[],int n)
         printf("%d ",a[i]);
     printf("\n");
 }
+//this is calling function using switch case.
 void Casehandle(int a[],int n,int size)
 {
    switch(n)
@@ -202,6 +71,150 @@ void Casehandle(int a[],int n,int size)
 
    }
 }
+//This is Bubble sort which time complexity worst case O(n^2).
+void Bubblesort(int a[],int n)
+{
+    int i,j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+                swep(&a[j],&a[j+1]);
+        }
+    }
+}
+//This is Selection sort time complexity O(n^2).
+void Selectionsort(int a[],int n)
+{
+    int i,j,min;
+    for(i=0;i<n-1;i++)
+    {
+        min=i;
+        for(j=i;j<n;j++)
+        {
+            if(a[j]<a[min])
+                min=j;
+        }
+        swep(&a[i],&a[min]);
+    }
+}
+//This is insertion sort time complexity O(n^2).
+void Insertionsort(int a[],int n)
+{
+    int i,j,x;
+    for(i=1;i<n;i++)
+    {
+        x=a[i];
+        j=i-1;
+        while(j>-1 && a[j]>x)
+        {
+           a[j+1]=a[j];
+           j--;
+        }
+        a[j+1]=x;
+    }
+}
+//This is Partition function for Quick sort.
+
+int partition(int a[],int l,int h)
+{
+    int pivot,i,j;
+    pivot=a[l];
+    i=l,j=h;
+    while(i<j)
+    {
+        do{
+            i++;
+        }while(a[i]<=pivot);
+        do{
+            j--;
+        }while(a[j]>pivot);
+        if(i<j)
+        swep(&a[i],&a[j]);
+    }
+    swep(&a[l],&a[j]);
+    return j;
+}
+//This is recursive Quick sort.
+void Quicksort(int a[],int l,int h)
+{
+    int j;
+    if(l<h)
+    {
+        j=partition(a,l,h);
+        Quicksort(a,l,j);
+        Quicksort(a,j+1,h);
+    }
+}
+//This is Marge function for Marging same array.
+void Marge(int a[],int l,int mid,int h)
+{
+    int i=l,j=mid+1,k=l;
+    int b[100];
+    while(i<=mid && j<=h)
+    {
+        if(a[i]<a[j])
+            b[k++]=a[i++];
+        else
+            b[k++]=a[j++];
+
+    }
+    for(;i<=mid;i++)
+        b[k++]=a[i];
+    for(;j<=h;j++)
+        b[k++]=a[j];
+    for(i=l;i<=h;i++)
+        a[i]=b[i];
+}
+//This recursive Marge sort function .
+void Margesort(int a[],int l,int h)
+{
+    int mid;
+    if(l<h)
+    {
+        mid=(l+h)/2;
+        Margesort(a,l,mid);
+        Margesort(a,mid+1,h);
+        Marge(a,l,mid,h);
+    }
+}
+//This index base Count sort function .This is faster sort .
+void Countsort(int a[],int n)
+{
+    int i,j,max;
+    max=Max(a,n)+1;
+    int c[max];
+    for(i=0;i<max;i++)
+        c[i]=0;
+    for(i=0;i<n;i++)
+        c[a[i]]++;
+    i=0,j=0;
+    while(i<max)
+    {
+        if(c[i]>0)
+        {
+            a[j++]=i;
+            c[i]--;
+        }
+       else
+            i++;
+    }
+}
+void print()
+{
+    printf("If want to exit enter 0 time....\n");
+    printf("Enter for 1 Bubble sort.O(n^2)..\n");
+    printf("Enter for 2 Selection sort O(n^2).\n");
+    printf("Enter for 3 Insertion sort.O(n^2).\n");
+    printf("Enter for 4 Quick sort.nlogn.\n");
+    printf("Enter for 5 Marge sort.nlogn;\n");
+    printf("Enter for 6 Count sort..O(n).\n");
+    printf("Enter for 7 Enter new array.\n");
+
+
+}
+
 int main()
 {
    int num,size,i;
